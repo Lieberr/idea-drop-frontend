64b183a37fab0111379289286f58fd629c449e1e
lieberr <gustavoliebfigueira@gmail.com>
2026-03-06 10:54:01 -0300

    Post and Delete mutation

--- ideia-drop-ui/src/routes/ideas/$ideaId/index.tsx
+++ ideia-drop-ui/src/routes/ideas/$ideaId/index.tsx
@@ -1,8 +1,8 @@
-import { createFileRoute, Link } from '@tanstack/react-router'
+import { createFileRoute, Link, useNavigate } from '@tanstack/react-router'
 import { queryOptions, useSuspenseQuery } from '@tanstack/react-query';
 import { fetchIdea } from '#/api/ideas';
-
-
+import { deleteIdea } from '#/api/ideas';
+import { useMutation } from '@tanstack/react-query';
 
 
 const ideaQueryOptions = (ideaId:string) => queryOptions({
@@ -23,6 +23,24 @@ export const Route = createFileRoute('/ideas/$ideaId/')({
 function IdeaDetailsPage() {
     const {ideaId} = Route.useParams();
     const {data:idea} = useSuspenseQuery(ideaQueryOptions(ideaId))
+    const navigate = useNavigate();
+
+    const {mutateAsync: deleteMutate, isPending} = useMutation({
+      mutationFn: () => deleteIdea(ideaId),
+      onSuccess: () => {
+        navigate({to: '/ideas'})
+      }
+    })
+
+
+    const handleDelete = async () => {
+      const confirmDelete = window.confirm('Are you sure you want to delete this idea? ');
+
+      if (confirmDelete) {
+        await deleteMutate();
+      }
+    }
+
   return <div className='p-4'>
     <Link to='/ideas' className='text-blue-500 underline block mb-4'>
       Back to Ideas
@@ -35,5 +53,13 @@ function IdeaDetailsPage() {
     <p className='mt-2'>
       {idea.description}
     </p>
+
+    <button
+    onClick={handleDelete}
+    disabled={isPending}
+    className='text-sm bg-red-600 text-white mt-4 px-4 py-2
+    rounded transition hover:bg-red-700 disabled:opacity:50'>
+      {isPending ? 'Deleting...' : 'Delete'}
+    </button>
   </div>
 }
--- ideia-drop-ui/src/routes/ideas/new/index.tsx
+++ ideia-drop-ui/src/routes/ideas/new/index.tsx
@@ -1,6 +1,7 @@
 import { createFileRoute, useNavigate } from '@tanstack/react-router'
-import { use, useState } from 'react'
-import type { Idea } from '#/types'
+import { useState } from 'react'
+import { createIdea } from '#/api/ideas'
+import { useMutation } from '@tanstack/react-query'
 
 export const Route = createFileRoute('/ideas/new/')({
   component: NewIdeaPage,
@@ -15,13 +16,45 @@ function NewIdeaPage() {
     const [tags, setTags] = useState('');
 
 
+    const {mutateAsync, isPending} = useMutation({
+        mutationFn: createIdea,
+        onSuccess: () => {
+            navigate({to: '/ideas'})
+        }
+    }) 
+
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+
+        if (!title.trim() || !description.trim() || !summary.trim()) {
+            alert('Please fill in all fields');
+            return;
+        }
+
+        try {
+            await mutateAsync({
+                title,
+                summary,
+                description,
+                tags: tags.split(',').map((tag) => tag.trim()).filter((tag) => tag !== '')
+            })
+        } catch (error) {
+            console.error(error)
+            alert('something went wrong')
+        }
+
+
+    }
+
+
   return (
     <div className='space-y-6'>
         <h1 className='text-3xl font-bold mb-6'>
             Create New Idea
         </h1>
 
-        <form className='space-y-4'>
+        <form onSubmit={handleSubmit} className='space-y-4'>
             <div>
                 <label 
                 htmlFor="title" 
@@ -89,6 +122,18 @@ function NewIdeaPage() {
                 placeholder='Optional tags, comma separated'
                 type="text"
                  />
+            </div>
+
+            <div className='mt-5'>
+                <button
+                type='submit'
+                disabled={isPending}
+                className='block w-full bg-blue-500 hover:bg-blue-700
+                text-white font-semibold px-6 py-2 rounded-md transition
+                disabled:opacity-50 disabled:cursor-not-allowed'
+                >
+                    {isPending ? 'Creating...' : 'Create Idea'}
+                </button>
             </div>
         </form>
     </div>
